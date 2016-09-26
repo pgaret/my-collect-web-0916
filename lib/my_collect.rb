ce8a834d0f2170458127be50f7cad30b9198d75b
Peregrin <peregringaret@gmail.com>
2016-09-26 01:41:57 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  array.collect do |x|
+    y = x.split(' ')
+    if y.length == 0
+      array
+    elsif y.length == 1
+      x = x.upcase
+    else
+      y[0]
+    end
+  end
+end
